4f07859227f5896af02b2d341f494568098637f9
adria-stef <adria.stef@gmail.com>
2014-07-09 12:15:32 +0300

    deleting old directories

--- 2014-05-29Problemss/src/com/hackbulgaria/corejava/Problems2Factory.java
+++
@@ -1,9 +0,0 @@
-package com.hackbulgaria.corejava;
-
-
-public class Problems2Factory {
-    public static Problems2 createProblems2Instance() {
-        //iska se da wyrnem Problems2impl 
-        return new Problems2Impl();
-    }
-}
